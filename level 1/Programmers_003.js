b030f41f1ccf0c6dde5f401b0aa44395d7e12eee
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-02 13:59:05 +0900

    없는 숫자 더하기

---
+++ level 1/Programmers_003.js
@@ -0,0 +1,10 @@
+// function solution(numbers) {
+//     let answer = -1
+//     for (let i = 1; i < 10; i++) {
+//         if 
+//     }
+//     return answer
+// }
+
+let array = [1, 2, 3]
+console.log(array.indexOf(4))
