879d5ff4c623f92f577958943f91fea4f922e2d8
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-02 13:50:35 +0900

    시저 암호
    2021-11-02

---
+++ level 1/programmers_002.js
@@ -0,0 +1,19 @@
+function solution(s, n) {
+    var answer = '';
+    for (let i = 0; i < s.length; i++) {
+        let temp = s.charCodeAt(i)
+        if (temp === 32) {
+            answer += ' '
+            continue
+        }
+        if ((temp <= 90 && temp + n > 90) || (temp <= 122 && temp + n > 122)) {
+            answer += String.fromCharCode(temp + n - 26)
+        }
+        else {
+            answer += String.fromCharCode(temp + n)
+        }
+    }
+    return answer;
+}
+
+console.log(solution('a B z', 4))
